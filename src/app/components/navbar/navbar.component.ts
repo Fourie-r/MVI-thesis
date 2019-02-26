b3ebbaef5124969beb5b7af72bbfa80027e73dad
Ivan <ivan.mihaylov1@haw-hamburg.de>
2019-02-26 16:10:45 +0100

    [ADD] - navbar chat notifications

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,11 +12,26 @@ export class NavbarComponent implements OnInit {
 
   public currentUser: any = null;
   public backlog = false;
+  public unread = false;
+
   @Input() drawer;
   constructor(public authService: AuthService, private chatroomService: ChatroomService) { }
 
   ngOnInit() {
-    this.authService.currentUser.subscribe( user => this.currentUser = user);
+    this.authService.currentUser.subscribe( user => {
+      this.currentUser = user;
+      if (user) {
+        this.chatroomService.getUnread(user).subscribe(chatrooms => {
+          let unread = false;
+          chatrooms.forEach(chatroom => {
+            if (chatroom['unread']) {
+              unread = true;
+            }
+            this.unread = unread;
+          });
+        });
+      }
+    });
   }
 
   public logout(): void {
